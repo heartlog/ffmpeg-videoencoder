f1b863cfddf1e12ba1e323d158cef831d6bb50d9
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 17:01:57 +0530

    Create __main__.py

---
+++ bot/__main__.py
@@ -0,0 +1,19 @@
+from pyrogram import filters
+from bot import app, data, sudo_users
+from bot.helper.utils import add_task
+
+video_mimetype = [
+  "video/x-flv",
+  "video/mp4",
+  "application/x-mpegURL",
+  "video/MP2T",
+  "video/3gpp",
+  "video/quicktime",
+  "video/x-msvideo",
+  "video/x-ms-wmv",
+  "video/x-matroska",
+  "video/webm",
+  "video/x-m4v",
+  "video/quicktime",
+  "video/mpeg"
+  ]
