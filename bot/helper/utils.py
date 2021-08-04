9921d80523a6750f42a53b594151fc50beafc5c2
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 17:05:01 +0530

    Create utils.py

---
+++ bot/helper/utils.py
@@ -0,0 +1,32 @@
+import os
+from bot import data, download_dir
+from pyrogram.types import Message
+from .ffmpeg_utils import encode, get_thumbnail, get_duration, get_width_height
+
+def on_task_complete():
+    del data[0]
+    if len(data) > 0:
+      add_task(data[0])
+
+def add_task(message: Message):
+    try:
+      msg = message.reply_text("```Downloading video Baka wait for some time !!...```", quote=True)
+      filepath = message.download(file_name=download_dir)
+      msg.edit("```started encoding video...```")
+      new_file = encode(filepath)
+      if new_file:
+        msg.edit("```Video Encoded, getting some data !```")
+        duration = get_duration(new_file)
+        thumb = get_thumbnail(new_file, download_dir, duration / 4)
+        width, height = get_width_height(new_file)
+        msg.edit("```Started to process and upload```")
+        message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
+        os.remove(new_file)
+        os.remove(thumb)
+        msg.edit("```Video Encoded to x265```")
+      else:
+        msg.edit("```Something wents wrong while encoding your file. Make sure it is not already in HEVC format.```")
+        os.remove(filepath)
+    except Exception as e:
+      msg.edit(f"```{e}```")
+    on_task_complete()
