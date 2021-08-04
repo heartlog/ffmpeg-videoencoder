c266bab1c55b90739a8292938cac5ccab06b0601
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 18:30:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-        api_id = int(os.environ.get("API_ID"))
+        api_id = int(os.environ.get("API_ID", "2176353"))
         api_hash = os.environ.get("API_HASH")
         bot_token = os.environ.get("BOT_TOKEN")
         download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
