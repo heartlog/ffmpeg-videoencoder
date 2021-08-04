d84419ce2a1e766891999835c1312a43b7542c6b
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 17:44:23 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,12 +3,10 @@ from pyrogram import Client
 from dotenv import load_dotenv
 from bot.configs import Config
 
-if os.path.exists('config.env'):
-  load_dotenv('config.env')
 
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_id = Config.api_id
+api_hash = Config.api_hash
+bot_token = Config.bot_token
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
 
