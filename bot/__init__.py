36afa0912c78c5e972754686338914871870cbe4
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 20:04:10 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -8,7 +8,6 @@ api_id = Config.api_id
 api_hash = Config.api_hash
 bot_token = Config.bot_token
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = "1771615392"
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 data = []
