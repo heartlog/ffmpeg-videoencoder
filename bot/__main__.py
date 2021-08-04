0676cd832e250f609d4b8ca82022cd555e9608ec
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-04 17:22:52 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,3 +21,26 @@ video_mimetype = [
 @app.on_message(filters.incoming & filters.command(['start', 'help']))
 def help_message(app, message):
     message.reply_text(f"Hi {message.from_user.mention()}\nIts bot to ensode video to ffmpeg encode..\nThe bot is developed by @diablo_13N\n"
+                       reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Clone this bot for free !",
+                                                                                  url="https://github.com/royal78/ffmpeg-cov")],
+                                                            [InlineKeyboardButton("My developer",
+                                                                                  url="t.me/diablo_13N")],
+                                                            [InlineKeyboardButton("Github page",
+                                                                                  url="https://github.com/royal78/ffmpeg-cov")],
+                                                            [InlineKeyboardButton("Join channel for updates",
+                                                                                  url="https://t.me/baka_no_onii")],
+                                                            [InlineKeyboardButton("Join group",
+                                                                                  url="https://t.me/anim_chatx")]]))
+
+@app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
+def encode_video(app, message):
+    if message.document:
+      if not message.document.mime_type in video_mimetype:
+        message.reply_text("```Invalid Video !\nMake sure its a valid video file.```", quote=True)
+        return
+    message.reply_text("```Added to queue !\n Wait for some time.```", quote=True)
+    data.append(message)
+    if len(data) == 1:
+      add_task(message)
+
+app.run()
