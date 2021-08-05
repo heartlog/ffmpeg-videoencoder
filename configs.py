829d1c66a937aad976e152a0cfd79efb69bfb1ad
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-05 09:09:59 +0530

    Delete configs.py

--- configs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-        api_id = int(os.environ.get("API_ID", "2176353"))
-        api_hash = os.environ.get("API_HASH")
-        bot_token = os.environ.get("BOT_TOKEN")
-        download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-        
