ad4bcd11a1a4ba5cff7c275c9cb59dea0491f4a9
Royal coolness <76758776+royal78@users.noreply.github.com>
2021-08-05 09:09:37 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,12 +1,10 @@
 import os
 from pyrogram import Client
-from dotenv import load_dotenv
-from configs import Config
 
 
-api_id = Config.api_id
-api_hash = Config.api_hash
-bot_token = Config.bot_token
+api_id = int(os.environ.get("api_id"))
+api_hash = os.environ.get("api_hash")
+bot_token = os.environ.get("bot_token")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
